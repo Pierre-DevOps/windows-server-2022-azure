b150ba663462a02cab96fa854717c3e22c59365a
Baroni Pierre <pierre.baroni@free.fr>
2025-12-04 20:58:59 +0100

    Fix: computer_name limité à 15 caractères

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -15,6 +15,7 @@ resource "azurerm_network_interface" "main" {
 # Windows Server 2022 Virtual Machine
 resource "azurerm_windows_virtual_machine" "main" {
   name                = "vm-${var.project_name}-${var.environment}"
+  computer_name       = "WINSERVER2022"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   size                = var.vm_size
